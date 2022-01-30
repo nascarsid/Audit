ae24ca3284962128aa602614fbe0c4040f68ab42
akshaydevh <akshaydevklm@gmail.com>
2022-01-30 23:52:09 +0530

    interface for LazyNft added

---
+++ Interface/IVizvaLazyNFT.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT;
+
+pragma solidity 0.8.2;
+
+interface ILazyNFT {
+    //represent the event emited after redeeming a voucher
+    event NFTRedeemed(uint256 tokenId, address minter, address redeemer);
+
+    /// @notice create new NFT.
+    /// @param minter address to which new token is minted.
+    /// @param tokenId id of the new token.
+    /// @param uri uri string for the new NFT.
+    function redeem(
+        address minter,
+        uint256 tokenId,
+        string memory uri
+    ) external;
+}
